464d4619e35509ab7e085cba951ab1036ace66e7
vasanthimgit <vasanthiadabala@Vasanthis-MacBook-Air.local>
2022-03-21 20:32:41 -0400

    Final Changes

---
+++ src/models/users.model.ts
@@ -0,0 +1,11 @@
+import { User } from '@interfaces/users.interface';
+
+// password: password
+const userModel: User[] = [
+  { id: 1, email: 'vasanthim@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
+  { id: 2, email: 'chandudev@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
+  { id: 3, email: 'DavidK@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
+  { id: 4, email: 'DonaldM@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
+];
+
+export default userModel;
